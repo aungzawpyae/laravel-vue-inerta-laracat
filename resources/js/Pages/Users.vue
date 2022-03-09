d834ffa5f73880e36b7c4707b8f7afa3ea6e373a
aungzawpyae <aungzawpyaeinf1001mm@gmail.com>
2022-03-09 23:23:13 +0630

    l-v-i

---
+++ resources/js/Pages/Users.vue
@@ -0,0 +1,86 @@
+
+
+<template>
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Users List
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                 <div class="flex flex-col">
+                    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+
+                                <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                                </th>
+
+                            </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200">
+                            <tr v-for="user in users.data" :key="user.id">
+            
+                                
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <span class="px-2 inline-flex text-xs leading-5 font-semibold ">
+                                    {{ user.id }}
+                                </span>
+                            </td>
+                            
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                {{ user.email }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                <Link :href=" '/users/' + user.id + '/edit' " class="text-indigo-600 hover:text-indigo-900">Edit</Link>
+                            </td>
+                            
+                            </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div v-for="link in users.links" :key="link.id">
+                    <Link  :is="link.url ? 'Link' : 'span'"
+                            v-if="link.url"
+                                :href="link.url"
+                                v-html="link.label"
+                                  class="px-1"
+      :class="{ 'text-gray-500': ! link.url, 'font-bold' : link.active }"
+                            >
+                                
+                    </Link>
+                    <span v-else v-html="link.label"></span>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
+
+<script >
+import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+import { Link } from '@inertiajs/inertia-vue3'
+
+//  defineProps({
+//   users: Object,
+// })
+export default {
+  props: {
+    users: Object
+  }
+ 
+}
+</script>
